635a83f378fe5e53e23babdfa80cc8a26b13188e
Harby-SoftwareEngineer <anabassem5@gmail.com>
2024-04-08 03:59:09 +0200

    init

--- lib/src/home/presentation/widgets/home_buttons.dart
+++ lib/src/home/presentation/widgets/home_buttons.dart
@@ -1,35 +1,53 @@
-
 import 'package:app/src/main_index.dart';
 
 import '../../../../core/utils/navigator.dart';
 import '../../../../core/widgets/texts/texts.dart';
 import '../../data/models/home_dto.dart';
 
-
 class HomeButtons extends BaseStatelessWidget {
   final List<Statistic> statistics;
-   HomeButtons({super.key, required this.statistics});
+
+  HomeButtons({super.key, required this.statistics});
+
   @override
   Widget build(BuildContext context) {
-
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Column(
       children: [
-        _Item(
-          icon: AppIcons.qayd_payment,
-          title: strings.qayd_pay,
-          onTap: (){
-            Navigators.pushNamed(Routes.qaydPaymentPage);
-          },
-        ),
-        _Item(
-          icon: AppIcons.reports,
-          title: strings.reports,
-          onTap: (){
-            Navigators.pushNamed(Routes.reportsPage);
-          },
-        ),
-      ]
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          _Item(
+            icon: AppIcons.qayd_payment,
+            title: strings.qayd_pay,
+            onTap: () {
+              Navigators.pushNamed(Routes.qaydPaymentPage);
+            },
+          ),
+          _Item(
+            icon: AppIcons.reports,
+            title: strings.reports,
+            onTap: () {
+              Navigators.pushNamed(Routes.reportsPage);
+            },
+          ),
+        ]),
+        10.ph,
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          _Item(
+            icon: AppIcons.qayd,
+            title: strings.register_qayd,
+    sizeIcon: 30,
+            onTap: () {
+              Navigators.pushNamed(Routes.regiserQaydPage);
+            },
+          ),
+          _Item(
+            icon: AppIcons.invoices,
+            title: strings.invoices,
+            onTap: () {
+              Navigators.pushNamed(Routes.invoicesPage);
+            },
+          ),
+        ]),
+      ],
     );
   }
 }
@@ -37,24 +55,27 @@ class HomeButtons extends BaseStatelessWidget {
 class _Item extends BaseStatelessWidget {
   final String icon;
   final String title;
-final Function()? onTap;
-   _Item({
+  final Function()? onTap;
+  final double? sizeIcon;
+
+  _Item({
     super.key,
     required this.icon,
     required this.title,
     required this.onTap,
+    this.sizeIcon,
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap:onTap ,
+      onTap: onTap,
       child: Container(
         margin: 5.paddingHoriz,
-        padding: 20.paddingVert,
+        padding: 18.paddingVert,
         constraints: const BoxConstraints(
           minWidth: 110,
-          maxHeight: 130,
+          maxHeight: 110,
         ),
         decoration: Decorations.baseDecorationRadius(),
         child: Column(
@@ -62,7 +83,8 @@ final Function()? onTap;
           children: [
             AppIcon(
               icon: icon,
-              size: 40,
+              size: sizeIcon ?? 40,
+              color: primaryColor,
             ),
             BoldText(
               label: title,
