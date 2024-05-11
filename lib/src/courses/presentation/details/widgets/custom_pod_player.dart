34327dd7671668b4dcdc549ef031e3c46b66aac8
Yahia <Yehyaelzaeem>
2024-05-11 23:22:58 +0300

    test2

--- lib/src/courses/presentation/details/widgets/custom_pod_player.dart
+++ lib/src/courses/presentation/details/widgets/custom_pod_player.dart
@@ -1,3 +1,5 @@
+import 'package:app/core/decorations/decorations.dart';
+import 'package:app/core/exceptions/extensions.dart';
 import 'package:pod_player/pod_player.dart';
 import 'package:flutter/material.dart';
 import '../../latest_courses/widgets/code_animation.dart';
@@ -46,39 +48,48 @@ class _PlayVideoFromNetworkState extends State<PlayVideoFromNetwork> {
                 onTapDown: (_) {
                   setState(() {
                     isFastForwarding = true;
+                    controller.videoSeekForward(const Duration(seconds: 5));
                   });
-                  controller.videoSeekForward(const Duration(seconds: 5));
                 },
                 onTapUp: (_) {
                   setState(() {
                     isFastForwarding = false;
                   });
                 },
-                child: SizedBox(
+                child: Container(
+                  decoration: Decorations.decorationStartEndBorder(
+                    topStart: 200,
+                    bottomStart: 200,
+                  ),
                   height: double.infinity,
-                  width: MediaQuery.of(context).size.width*0.45,
+                  width: MediaQuery.of(context).size.width*0.40,
                   child: isFastForwarding
                       ? const Icon(Icons.fast_forward,color: Colors.white,) // Show fast forward icon when double-tapped
                       : Container(),
                 ),
               ),
               InkWell(
+
                 onTapDown: (_) {
                   setState(() {
                     isFastBackward = true;
+                    controller.videoSeekBackward(const Duration(seconds: 5));
                   });
-                  controller.videoSeekBackward(const Duration(seconds: 5));
                 },
                 onTapUp: (_) {
                   setState(() {
                     isFastBackward = false;
                   });
                 },
-                child: SizedBox(
+                child: Container(
+                  decoration: Decorations.decorationStartEndBorder(
+                      topEnd: 200,
+                      bottomEnd: 200,
+                  ),
                   height: double.infinity,
-                  width: MediaQuery.of(context).size.width*0.45,
+                  width: MediaQuery.of(context).size.width*0.40,
                   child: isFastBackward
-                      ? const Icon(Icons.fast_rewind,color: Colors.white,) // Show fast forward icon when double-tapped
+                      ? const Icon(Icons.fast_rewind,color: Colors.white,size: 30,) // Show fast forward icon when double-tapped
                       : Container(),
                 ),
               )
@@ -96,10 +107,11 @@ class _PlayVideoFromNetworkState extends State<PlayVideoFromNetwork> {
               Icon(
                 controller.isVideoPlaying==true?
                 Icons.pause:Icons.play_arrow,
-                color: Colors.white,)),
+                color: Colors.white,size: 45,)),
             ),
-            Align(
-              alignment: Alignment.bottomLeft,
+            Positioned(
+              bottom: 5,
+              left: 0,
               child: IconButton(
                 onPressed: (){
                   if(controller.isFullScreen){
@@ -115,10 +127,39 @@ class _PlayVideoFromNetworkState extends State<PlayVideoFromNetwork> {
                   Icons.fullscreen,color: Colors.white,),
               ),
             ),
+            Positioned(
+              bottom: 20,
+              right: 15,
+              child:
+             Column(
+               children: [
+                 Text('${formatTime(controller.totalVideoLength.toString())}/${formatTime(controller.currentVideoPosition.toString())}',style: TextStyle(color: Colors.white),),
+               ],
+             ),
+            ),
             CodeAnimation(),
           ],
         );
       }
     );
   }
-}
+  String formatTime(String timeString) {
+    List<String> parts = timeString.split(':'); // تقسيم السلسلة الزمنية بواسطة ':'
+
+    int hours = int.parse(parts[0]); // استخراج الساعات
+    int minutes = int.parse(parts[1]); // استخراج الدقائق
+    double seconds = double.parse(parts[2]); // استخراج الثواني
+
+    String formattedTime;
+
+    if (hours == 0) {
+      formattedTime = '${minutes.toString().padLeft(2, '0')}:${seconds.toStringAsFixed(0).padLeft(2, '0')}';
+    } else {
+      formattedTime = '${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toStringAsFixed(0).padLeft(2, '0')}';
+    }
+
+    return formattedTime; // إرجاع السلسلة الزمنية المنسقة
+  }
+  }
+
+
--- lib/src/test2.dart
+++ lib/src/test2.dart
@@ -13,13 +13,20 @@ class _Test2State extends State<Test2> {
    final String url =
        'https://www.youtube.com/watch?v=m3GEMLMbuKA&list=RDm3GEMLMbuKA&start_radio=1'
    ;
+   PodPlayerController? controller2;
+
    PodPlayerController? controller;
    @override
   void initState() {
      controller=  PodPlayerController(
        playVideoFrom: PlayVideoFrom.youtube(
            url    ),
-     )..initialise();    super.initState();
+     )..initialise();
+     controller2=  PodPlayerController(
+       playVideoFrom: PlayVideoFrom.youtube(
+           'https://www.youtube.com/watch?v=L4MTSb85fog&list=RDL4MTSb85fog&start_radio=1'    ),
+     )..initialise();
+     super.initState();
   }
 
 
@@ -32,6 +39,9 @@ class _Test2State extends State<Test2> {
         children: [
           const Center(child: Text('بسم الله الرحمن الرحيم')),
           100.ph,
+          // PodVideoPlayer(
+          //   controller: controller2!,),
+          50.ph,
           PlayVideoFromNetwork(
             url:url,
             controller: controller,
