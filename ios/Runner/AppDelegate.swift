236e7dd253fbe28d6fbb6a42abe290e64821b945
harbey127 <harbey127@gmail.com>
2024-05-21 00:11:52 +0300

    init project

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,20 +9,20 @@ import FirebaseCore
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
-    self.window.makeSecure()
+//     self.window.makeSecure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
 
-extension UIWindow {
-func makeSecure() {
-    let field = UITextField()
-    field.isSecureTextEntry = true
-    self.addSubview(field)
-    field.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-    field.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-    self.layer.superlayer?.addSublayer(field.layer)
-    field.layer.sublayers?.first?.addSublayer(self.layer)
-  }
-}
+// extension UIWindow {
+// func makeSecure() {
+//     let field = UITextField()
+//     field.isSecureTextEntry = true
+//     self.addSubview(field)
+//     field.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+//     field.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+//     self.layer.superlayer?.addSublayer(field.layer)
+//     field.layer.sublayers?.first?.addSublayer(self.layer)
+//   }
+// }
