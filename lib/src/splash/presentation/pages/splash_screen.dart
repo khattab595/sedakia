5edafa71e2cbf3da4afc2d261a82cd8925e526a1
khattab <akhattab595@gmail.com>
2024-05-22 01:07:22 +0300

    kattab

--- lib/src/splash/presentation/pages/splash_screen.dart
+++ lib/src/splash/presentation/pages/splash_screen.dart
@@ -19,7 +19,7 @@ class _SplashPageState extends State<SplashPage>
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: const Duration(seconds: 2),
+      duration: const Duration(seconds: 5),
       vsync: this,
     )..repeat(reverse: true);
 
