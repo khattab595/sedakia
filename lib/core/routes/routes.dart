65e1a2f7f60099dadd44f7c82180ebec8829137b
khattab <akhattab595@gmail.com>
2024-10-18 23:16:58 +0300

    kattab

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,20 +1,18 @@
- import '../../src/auth/presentation/pages/forget-password/enter_pin_code_page.dart';
+import '../../src/auth/presentation/pages/forget-password/enter_pin_code_page.dart';
 import '../../src/auth/presentation/pages/forget-password/change_password_page.dart';
- import '../../src/auth/presentation/pages/login/login_page.dart';
+import '../../src/auth/presentation/pages/login/login_page.dart';
 import '../../src/categories/presentation/pages/add/add_categories_page.dart';
 import '../../src/categories/presentation/pages/view/categories_page.dart';
 import '../../src/home/presentation/pages/home_page.dart';
- import '../../src/main_index.dart';
+import '../../src/main_index.dart';
 import '../../src/nav_bar/presentation/pages/main_navgation_bar.dart';
-  import '../../src/product/presentation/pages/add/add_product_page.dart';
+import '../../src/product/presentation/pages/add/add_product_page.dart';
 import '../../src/product/presentation/pages/view/product_page.dart';
 import '../../src/profile/presentation/pages/profile_page.dart';
- import '../../src/settings/presentation/pages/change-language/chang_language_page.dart';
- import '../../src/splash/presentation/pages/splash_screen.dart';
+import '../../src/settings/presentation/pages/change-language/chang_language_page.dart';
+import '../../src/splash/presentation/pages/splash_screen.dart';
 
 class Routes {
-
-
   static const String intro = '/';
   static const String loginPage = 'loginUI';
 
@@ -23,26 +21,25 @@ class Routes {
   static const String changePasswordPage = 'ChangePasswordPage';
   static const String changLanguagePage = 'ChangLanguagePage';
 
-   static const String enterPinCodePage = 'enterPinCodePage';
+  static const String enterPinCodePage = 'enterPinCodePage';
   static const String newPasswordPage = 'newPasswordPage';
   static const String navigationPages = 'navigationPages';
-   static const String profile = 'profile';
+  static const String profile = 'profile';
   static const String settings = 'settings';
-   static const String homePage = 'homePage';
-   static const String splashPage = 'splashPage';
-   static const String productPage = 'productPage';
-   static const String addProductPage = 'addProductPage';
-   static const String categoriesPage = 'categoriesPage';
-   static const String addCategoriesPage = 'addCategoriesPage';
-
+  static const String homePage = 'homePage';
+  static const String splashPage = 'splashPage';
+  static const String productPage = 'productPage';
+  static const String addProductPage = 'addProductPage';
+  static const String categoriesPage = 'categoriesPage';
+  static const String addCategoriesPage = 'addCategoriesPage';
 
   /* ====================================================================== */
   static const String productDetailsPage = 'productDetailsPage';
 
   static final routes = <String, Widget Function(BuildContext)>{
     splashPage: (context) => SplashPage(),
-       loginPage: (context) => LoginPage(),
-     changePasswordPage: (context) => ChangePasswordPage(),
+    loginPage: (context) => LoginPage(),
+    changePasswordPage: (context) => ChangePasswordPage(),
     enterPinCodePage: (context) => EnterPinCodePage(),
     navigationPages: (context) => NavigationBarPage(),
     homePage: (context) => HomePage(),
@@ -52,5 +49,5 @@ class Routes {
     addProductPage: (context) => AddProductPage(),
     categoriesPage: (context) => CategoriesPage(),
     addCategoriesPage: (context) => AddCategoriesPage(),
-   };
+  };
 }
