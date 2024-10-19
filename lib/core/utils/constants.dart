a8a9705e7c25d36c7718dc35759607fb9faec8c9
Mohamed768830 <mz2139006@gmail.com>
2024-10-19 17:13:40 +0300

    gddjgjgd

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -2,9 +2,9 @@
 const String kMaterialAppTitle = 'Flutter Clean Architecture';
 
 // API
-const String kTestBaseUrl = 'https://alwaseet-sa.online/api/';
+const String kTestBaseUrl = 'https://mm.motkaml.online/wp-json/';
 const String kApiKey = 'ff957763c54c44d8b00e5e082bc76cb0';
- const String baseApiUrl = "https://alwaseet-sa.online/api/";
+ const String baseApiUrl = "https://mm.motkaml.online/wp-json/";
 
 const kBaseUrl = kIsTestVersion ? baseApiUrl : kTestBaseUrl;
 
